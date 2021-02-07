ccd6db8d4802f801396c4b28ffba0668b0206a54
Rita Sanchez <rita@Mac-mini-de-Rita.local>
2021-02-07 18:18:19 +0100

    Adding all files at once

---
+++ analysis.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+load("rdas/murders.rda")
+
+str(murders)
+
+murders %>% mutate(abb = reorder(abb, rate)) %>% 
+  ggplot(aes(abb, rate))+
+  geom_bar(width = 0.5, stat = "identity", color = "black")+
+  coord_flip()
+
+ggsave("figs/barplot.png")
